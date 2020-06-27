2c64cbf4bbb1c5792f73cc9f4f61d3205a056148
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-06-27 01:29:50 +0100

    style

--- packages/shop/src/components/CardType/type-card.style.tsx
+++ packages/shop/src/components/CardType/type-card.style.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const CardTypeWrapper = styled.div`
+export const CardTypeWrapper = styled.button`
   /* display: block; */
   display: flex;
   width: 100%;
@@ -14,9 +14,20 @@ export const CardTypeWrapper = styled.div`
 
   border-color: ${(props) => props.color || "#CCCCCC"};
   box-shadow: 1px 1px 0px 0px rgba(0, 0, 255, 0.2);
+
+  &:hover {
+    border-width: 2px;
+    border-color: ${(props) => props.color || "#009e7f"};
+    box-shadow: 0 5px 5px rgba(17, 16, 62, 0.15);
+  }
+
+  &:focus {
+    border-color: ${(props) => props.color || "#009e7f"};
+    border-radius: 6px;
+  }
 `;
 
-export const Title = styled.h3({
+export const Title = styled.h2({
   color: "#50536E",
 });
 
@@ -26,7 +37,7 @@ export const Icon = styled.span({
   color: "#50536E",
 });
 
-export const Description = styled.h6({
+export const Description = styled.h5({
   marginTop: "0.3rem",
   color: "#77798C",
 });
