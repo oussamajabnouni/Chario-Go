ebcf27d029720fe22ed8f3bb6349ae7070f4170c
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-07-08 15:57:32 +0100

    footer

--- packages/shop/src/components/restaurant-filters/restaurant-filters.tsx
+++ packages/shop/src/components/restaurant-filters/restaurant-filters.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { FiltersWrapper, Row } from "./restaurant-filters.style";
+import { FiltersWrapper, Row, RowInput } from "./restaurant-filters.style";
+import { FormattedMessage } from "react-intl";
 import Select from "../select/select";
-
+import Input from "../input/input";
+import { Button } from "../button/button";
 interface Props {
   onEnter: (e: React.SyntheticEvent) => void;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder?: string;
+
   value: string;
   name: string;
   minimal?: boolean;
@@ -23,11 +27,20 @@ const RestaurantFilters: React.FC<Props> = () => {
       <Row>
         <Select inputValue="" />
       </Row>
-      <Row>
-        <Select inputValue="" />
-      </Row>
+      <RowInput>
+        <Input
+          type="text"
+          intlPlaceholderId="searchPlaceholder"
+          style={{ height: 57 }}
+        />
+      </RowInput>
       <Row>
         <Select inputValue="Trier" />
+      </Row>
+      <Row>
+        <Button variant="outlined" style={{ height: "100%", width: "100%" }}>
+          <FormattedMessage id="searchButtonText" defaultMessage="Search" />
+        </Button>
       </Row>
     </FiltersWrapper>
   );
