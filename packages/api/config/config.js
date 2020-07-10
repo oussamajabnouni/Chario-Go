e9c0fbac79e109373d00df7c235c4a1c4cf59a12
Oussama Jabnouni <osama.jbnoni@gmail.com>
2020-07-10 18:43:45 +0100

    fix mysql

--- packages/api/config/config.js
+++ packages/api/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "mysql",
+    "dialect": process.env.DB_DIALECT,
     "port": process.env.DB_PORT
   },
   "test": {
