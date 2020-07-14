c22efdd65376a96cbed349f26402a9022791d628
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-07-14 17:45:30 +0100

    ChangeLogo

--- packages/shop/src/layouts/header/header.tsx
+++ packages/shop/src/layouts/header/header.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Router, { useRouter } from 'next/router';
-import { openModal } from '@redq/reuse-modal';
-import { AuthContext } from 'contexts/auth/auth.context';
-import AuthenticationForm from 'features/authentication-form';
-import { RightMenu } from './menu/right-menu/right-menu';
-import { LeftMenu } from './menu/left-menu/left-menu';
-import HeaderWrapper from './header.style';
-import LogoImage from 'assets/images/logo.svg';
-import UserImage from 'assets/images/user.jpg';
-import { isCategoryPage } from '../is-home-page';
-import Search from 'features/search/search';
+import React from "react";
+import Router, { useRouter } from "next/router";
+import { openModal } from "@redq/reuse-modal";
+import { AuthContext } from "contexts/auth/auth.context";
+import AuthenticationForm from "features/authentication-form";
+import { RightMenu } from "./menu/right-menu/right-menu";
+import { LeftMenu } from "./menu/left-menu/left-menu";
+import HeaderWrapper from "./header.style";
+import LogoImage from "assets/images/charioGo.png";
+import UserImage from "assets/images/user.jpg";
+import { isCategoryPage } from "../is-home-page";
+import Search from "features/search/search";
 type Props = {
   className?: string;
 };
@@ -21,34 +21,34 @@ const Header: React.FC<Props> = ({ className }) => {
   } = React.useContext<any>(AuthContext);
   const { pathname, query } = useRouter();
   const handleLogout = () => {
-    if (typeof window !== 'undefined') {
-      localStorage.removeItem('access_token');
-      authDispatch({ type: 'SIGN_OUT' });
-      Router.push('/');
+    if (typeof window !== "undefined") {
+      localStorage.removeItem("access_token");
+      authDispatch({ type: "SIGN_OUT" });
+      Router.push("/");
     }
   };
 
   const handleJoin = () => {
     authDispatch({
-      type: 'SIGNIN',
+      type: "SIGNIN",
     });
 
     openModal({
       show: true,
-      overlayClassName: 'quick-view-overlay',
+      overlayClassName: "quick-view-overlay",
       closeOnClickOutside: true,
       component: AuthenticationForm,
-      closeComponent: '',
+      closeComponent: "",
       config: {
         enableResizing: false,
         disableDragging: true,
-        className: 'quick-view-modal',
+        className: "quick-view-modal",
         width: 458,
-        height: 'auto',
+        height: "auto",
       },
     });
   };
-  const type = pathname === '/restaurant' ? 'restaurant' : query.type;
+  const type = pathname === "/restaurant" ? "restaurant" : query.type;
   const showSearch = isCategoryPage(type);
   return (
     <HeaderWrapper className={className} id="layout-header">
